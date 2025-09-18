7ee8e088797c645b43baf00cbab7dad80374b75a
Jenypr <jenyffer.d@escolar.ifrn.edu.br>
2025-09-18 14:19:40 +0000

    agendamento

--- Agenda/index.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from retanguloUI import RetanguloUI
-
-RetanguloUI.main()
-
-#st.header("Streamlit em Python")
-#if st.button("Clique Aqui"):
-#    st.write("Minha primeira página em Python")
